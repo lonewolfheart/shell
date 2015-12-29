c5d04176fc0e7f778e5ca3f836a1010c3c6d9254
Dragon Tong <lonewolfs@163.com>
2015-12-29 14:37:56 +0800

    shell variables script

---
+++ var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#define path variables
+#by authors lonewolf 2015
+
+name=lonewolf
+echo "The script author is $name!"
+echo -e "\033[32m----------------\033[0m"
+echo "1.install nginx"
+echo "2.install mysql"
+echo "3.install tomcat"
+echo "4.config them"
+echo -e "\033[32m----------------\033[0m"
